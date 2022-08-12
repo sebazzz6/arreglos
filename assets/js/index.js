5b2188e41cf7908d072361daa770b542ee34bb85
sebazzz6 <sebastian.zuniga.lob@gmail.com>
2022-08-12 17:03:11 -0400

    INFO: add folder and files

---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+const sectionCard = document.querySelector("#prueba");
+
+const propiedadesJSON = [{name: "Casa de campo", description: "Un lugar ideal para descansar de la ciudad",
+                          src: "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+                          rooms: 2, m: 170},
+                        {name: "Casa de playa", description: "Despierta tus días oyendo el oceano",
+                          src: "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+                          rooms: 2, m: 130},
+                        {name: "Casa en el centro", description: "Ten cerca de ti todo lo que necesitas",
+                          src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+                          rooms: 1, m: 80},
+                        {name: "Casa rodante", description: "Conviertete en un nómada del mundo sin salir de tu casa",
+                          src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+                          rooms: 1, m: 6},
+                        {name: "Departamento", description: "Desde las alturas todo se ve mejor",
+                          src: "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+                          rooms: 3, m: 200},
+                        {name: "Mansión", description: "Vive una vida lujosa en la mansión de tus sueños ",
+                          src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+                          rooms: 5, m: 500},];
+
+function setCounter(params){
+  let counter= document.querySelector("#counter").innerHTML= params;
+}
+
+function initialLoader() {
+  sectionCard.innerHTML = "";
+  let roomInput = (document.querySelector("#cuartosInput").value = "");
+  let fromInput = (document.querySelector("#desdeInput").value = "");
+  let toInput = (document.querySelector("#hastaInput").value = "");
+  for (const propiedad of propiedadesJSON) {
+    sectionCard.innerHTML += `<div class="propiedad"><div class="img" style="background-image: url('${propiedad.src}')"></div><section><h5 >${propiedad.name}</h5>
+                            <div class="d-flex justify-content-between"><p>${propiedad.rooms}</p><p>${propiedad.m}</p></div><p class="my-3">${propiedad.description}</p>
+                            <button class="btn btn-info ">Ver más</button></section></div>`;
+  }
+  setCounter(propiedadesJSON.length);
+}
+
+
+initialLoader();
+
+function filterProperties() {
+  let roomInput = document.querySelector("#cuartosInput").value;
+  let fromInput = document.querySelector("#desdeInput").value;
+  let toInput = document.querySelector("#hastaInput").value;
+  if (!validateInput(roomInput) || !validateInput(fromInput) || !validateInput(toInput)) {
+    alert("Se debe completar todos los campos");
+    return;
+  }
+  let filteredproperties= propiedadesJSON.filter((propiedad) => propiedad.rooms == roomInput && propiedad.m >= fromInput && propiedad.m <= toInput);
+  sectionCard.innerHTML = "";
+  if (filteredproperties.length == 0) {
+    alert("No se encontró alguna propiedad en base a los filtros");
+    initialLoader();
+    return;
+  }
+  for (const propiedad of filteredproperties) {
+    sectionCard.innerHTML += `<div class="propiedad"><div class="img" style="background-image: url('${propiedad.src}')"></div><section>
+                    <h5 >${propiedad.name}</h5><div class="d-flex justify-content-between"><p>${propiedad.rooms}</p><p>${propiedad.m}</p>
+                    </div><p class="my-3">${propiedad.description}</p><button class="btn btn-info ">Ver más</button></section></div>`;
+  }
+  setCounter(filteredproperties.length)
+}
+
+
+function validateInput(params) {
+  if (params !== undefined && params !== null && params !== "" && params !== " " ) {
+    return true;
+  }
+  return false;
+}
